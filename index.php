4fdb646e9b5d8cf5c331189725bb5c1c475c0ded
Nilson360 <nilsonsimao36@gmail.com>
2022-12-05 14:32:16 +0100

    update

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My System Login</title>
+</head>
+<body>
+    <h1>Wellcome, usermae</h1>
+</body>
+</html>
